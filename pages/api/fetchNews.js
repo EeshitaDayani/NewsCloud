198c48a5d3133ef208f93598ff34bfb07e61e710
Eeshita Dayani <eeshitadayani.gmail.com>
2023-10-15 19:46:01 -0400

    NewAPI data reflects in Word Cloud

--- pages/api/fetchNews.js
+++ pages/api/fetchNews.js
@@ -1,40 +1,48 @@
-const fs = require('fs');
+const { NextApiRequest, NextApiResponse } = require('next');
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('83780bc6c4234075bd7ed62bd2d1276c');
-const numArticles = 25;
+
+const newsapi = new NewsAPI(process.env.NEXT_PUBLIC_NEWS_API);
+const numArticles = 20;
+
+const todayDate = new Date();
+const lastWeekDate = new Date();
+lastWeekDate.setDate(todayDate.getDate() - 7);
 
 // Import the configuration from the JSON file
 const config = {
   "q": "usa",
   "sources": "bbc-news",
   "searchIn": "title,description",
-  "from": new Date(new Date().setDate(today.getDate() - 30)).toISOString().slice(0, 10),
-  "to": new Date().toISOString().slice(0, 10),
+  "from": lastWeekDate.toISOString().slice(0, 10),
+  "to": todayDate.toISOString().slice(0, 10),
   "language": "en",
   "sortBy": "popularity",
-  "pageSize": 15,
+  "pageSize": numArticles,
   "page": 1
 }
 
-newsapi.v2.everything({
-  q: config.q,
-  sources: config.sources,
-  from: config.from,
-  to: config.to,
-  language: config.language,
-  sortBy: config.sortBy,
-  pageSize: config.pageSize,
-  page: config.page
-}).then(response => {
-  // Write the response to output.json
-  const anyChartData = response.articles.map((article, index) => ({
-    x: article.title,
-    custom_field: article.description,
-    value: (numArticles - index) * numArticles  // Assuming a pattern where the first article has a value of 15, second has 0, and so on
-  }));
+export default async function handler(req, res) {
+  try {
+    const response = await newsapi.v2.everything({
+      q: config.q,
+      sources: config.sources,
+      from: config.from,
+      to: config.to,
+      language: config.language,
+      sortBy: config.sortBy,
+      pageSize: config.pageSize,
+      page: config.page
+    });
 
-  fs.writeFile('output.json', JSON.stringify(anyChartData), err => {
-    if (err) throw err;
-    console.log('Response has been saved to output.json');
-   });
-});
+    const data = response.articles.map((article, index) => ({
+      title: article.title
+    }));
+
+    res.status(200).json({ data: data });
+  
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -1,53 +1,15 @@
 import React, { useRef, useState, useMemo, useEffect } from 'react';
 import * as THREE from 'three';
-
 import Word from './Word';
 
-var colorArr = [  'rgb(255, 0, 0)',   
-                  'rgb(255, 13, 0)',
-                  'rgb(255, 27, 0)', 
-                  'rgb(255, 40, 0)',
-                  'rgb(255, 54, 0)',  
-                  'rgb(255, 67, 0)',
-                  'rgb(255, 81, 0)',  
-                  'rgb(255, 94, 0)',
-                  'rgb(255, 107, 0)', 
-                  'rgb(255, 121, 0)',
-                  'rgb(255, 134, 0)', 
-                  'rgb(255, 148, 0)',
-                  'rgb(255, 161, 0)', 
-                  'rgb(255, 174, 0)',
-                  'rgb(255, 188, 0)', 
-                  'rgb(255, 201, 0)',
-                  'rgb(255, 215, 0)', 
-                  'rgb(255, 228, 0)',
-                  'rgb(255, 242, 0)', 
-                  'rgb(255, 255, 0)'];
+const colorArr = [];
+for (let i = 0; i < 20; i++) {
+  const color = `rgb(255, ${Math.min(255, i * 13)}, 0)`;
+  colorArr.push(color);
+}
 
-const newsPhrases = [
-    'Global Leaders Announce Climate Change Action Plan Today',
-    'Tech Giants Unveil Breakthrough Artificial Intelligence Innovations',
-    'World Health Organization Addresses Ongoing Public Health Concerns',
-    'Financial Markets React to Unprecedented Economic Policy Changes',
-    'Space Exploration Mission Reveals Surprising Discoveries on Mars',
-    'Major International Summit Focuses on Peace and Diplomacy',
-    'Groundbreaking Study Offers New Insights into Cancer Research',
-    'Environmental Activists Rally for Sustainable Practices Worldwide',
-    'Political Turmoil Continues as Elections Approach in Key Regions',
-    'Investigation Uncovers Corporate Scandals in Banking and Finance',
-    'New Technology Promises Revolutionary Advances in Clean Energy',
-    'Humanitarian Crisis Escalates in Conflict-Stricken Regions',
-    'Innovative Education Initiatives Transform Learning for Students',
-    'Crisis in the Middle East Sparks Global Security Concerns',
-    'Breaking News: Celebrity Philanthropist Launches Charity Foundation',
-    'Advancements in Medical Research Bring Hope for Rare Diseases',
-    'Government Initiatives Tackle Rising Unemployment Rates',
-    'Entertainment Industry Faces Challenges Amid Changing Consumer Trends',
-    'Global Economy Faces Uncertainty with Trade Agreement Negotiations',
-    'Scientific Community Debates Ethical Implications of Genetic Engineering'
-  ]
-
-export default function Cloud({ count = 20, radius = 20 }) {
+export default function Cloud({ count = 20, radius = 20, newsPhrases }) {
+  // const newsPhrases = await getNewsPhrases().data;
     const words = useMemo(() => {
       const temp = []
       const spherical = new THREE.Spherical()
