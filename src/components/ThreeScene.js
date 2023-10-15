903f23a8204a23c2a981d42bd501caf5f3eedf8a
Eeshita Dayani <eeshitadayani.gmail.com>
2023-10-15 19:46:38 -0400

    Shifted API call to ThreeScene

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -3,11 +3,20 @@ import React, { useRef, useState, useMemo, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Text, TrackballControls } from '@react-three/drei';
 import * as THREE from 'three';
+import useSWR from 'swr';
 
 import Cloud from './Cloud';
 
+const fetcher = (url) => fetch(url).then(r => r.json())
+
 export default function ThreeScene() {
+
+  const { data, error } = useSWR('/api/fetchNews', fetcher);
+
+  const headlines = data ? data.data.map(item => item.title) : [];
+
   return (
+    data ? 
     <Canvas 
     onCreated={({ gl }) => {
         gl.setClearColor(new THREE.Color('#000000')); // Set background color to black
@@ -16,8 +25,14 @@ export default function ThreeScene() {
     dpr={[1, 2]}
     camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}>
       <fog attach="fog" args={['#000000', 0, 100]} />
-      <Cloud count={20} radius={40} />
+      <Cloud count={20} radius={40} newsPhrases={ headlines } />
       <TrackballControls />
     </Canvas>
+    :
+    <Canvas
+    onCreated={({ gl }) => {
+      gl.setClearColor(new THREE.Color('#000000')); // Set background color to black
+    }}
+    ></Canvas>
   );
 }
