36df1b7ef725181ad4af776322cea8735e931c6c
Eeshita Dayani <eeshitadayani@gmail.com>
2023-11-05 12:55:01 -0500

    Added loading screen for ThreeScene component

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -14,19 +14,36 @@ const fontProps = { fontSize: 5.5,
                     lineHeight: 1, 
                     'material-toneMapped': false, 
                     color: 'white'}
+// ... (imports)
 
 export default function ThreeScene({ searchQuery, date, isMobile }) {
-  const { data } = useSWR(`/api/fetchNews?q=${searchQuery}&from=${getDate(date)}`, fetcher);
+  const { data, isValidating } = useSWR(`/api/fetchNews?q=${searchQuery}&from=${getDate(date)}`, fetcher);
   const { error } = data ? data : '';
-  const headlines = data && data.data ? data.data.map(item => ({'title': item.title, 'url': item.url})) : [];
+  const headlines = data && data.data ? data.data.map(item => ({ 'title': item.title, 'url': item.url })) : [];
+
+  if (isValidating || !data) {
+    // Show loading screen while data is being fetched
+    return (
+      <Canvas
+        onCreated={({ gl }) => gl.setClearColor(new THREE.Color('#121212'))}
+        style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
+        dpr={[1, 2]}
+        camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}
+      >
+        <fog attach="fog" args={['#121212', 0, 100]} />
+        <Text children="Loading..." {...fontProps} />
+      </Canvas>
+    );
+  }
 
   if (error) {
     return (
-      <Canvas onCreated={({ gl }) => {
-                            gl.setClearColor(new THREE.Color('#121212'))}}
-              style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
-              dpr={[1, 2]}
-              camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}>
+      <Canvas
+        onCreated={({ gl }) => gl.setClearColor(new THREE.Color('#121212'))}
+        style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
+        dpr={[1, 2]}
+        camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}
+      >
         <fog attach="fog" args={['#121212', 0, 100]} />
         <Text children="Sorry, an error occurred." {...fontProps} />
       </Canvas>
@@ -35,27 +52,28 @@ export default function ThreeScene({ searchQuery, date, isMobile }) {
 
   if (headlines.length === 0) {
     return (
-      <Canvas onCreated={({ gl }) => {
-                            gl.setClearColor(new THREE.Color('#121212'))}}
-              style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
-              dpr={[1, 2]}
-              camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}>
+      <Canvas
+        onCreated={({ gl }) => gl.setClearColor(new THREE.Color('#121212'))}
+        style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
+        dpr={[1, 2]}
+        camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}
+      >
         <fog attach="fog" args={['#121212', 0, 100]} />
         <Text children="No search results" {...fontProps} />
       </Canvas>
-    )
+    );
   }
-  
 
   return (
-    <Canvas onCreated={({ gl }) => {
-                          gl.setClearColor(new THREE.Color('#121212'))}}
-            style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
-            dpr={[1, 2]}
-            camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}>
+    <Canvas
+      onCreated={({ gl }) => gl.setClearColor(new THREE.Color('#121212'))}
+      style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
+      dpr={[1, 2]}
+      camera={{ position: [0, 0, 70], fov: 90, near: 1, far: 200 }}
+    >
       <fog attach="fog" args={['#121212', 0, 100]} />
-      <Cloud radius={40} headlines={ headlines } isMobile={isMobile}/>
+      <Cloud radius={40} headlines={headlines} isMobile={isMobile} />
       <TrackballControls />
     </Canvas>
-  )
+  );
 }
