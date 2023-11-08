170c6f680500e4ea90779d4e5ac13d4749180347
Eeshita Dayani <eeshitadayani@gmail.com>
2023-11-07 23:30:02 -0500

    Temporarily removed Date Picker from Dropdown menu

--- src/components/DateRangeSelector.js
+++ src/components/DateRangeSelector.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import dayjs from 'dayjs';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const darkTheme = createTheme({
   palette: {
@@ -30,6 +33,12 @@ export default function DateRangeSelector({ onSelect }) {
         }
       };
 
+      const [selectedDate, setSelectedDate] = useState(null);
+
+      const handleDateChange = (date) => {
+        setSelectedDate(date);
+      };
+
       return (
         
         <Box sx={{ minWidth: 120 }}>
@@ -51,7 +60,16 @@ export default function DateRangeSelector({ onSelect }) {
               >
                 <MenuItem value="lastWeek">Last Week</MenuItem>
                 <MenuItem value="lastMonth">Last Month</MenuItem>
-                <MenuItem value="custom">Custom</MenuItem>
+                {/* <MenuItem value="custom">
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="Custom"
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                      renderInput={(params) => <div {...params} />}
+                    />
+                  </LocalizationProvider>
+                </MenuItem> */}
               </Select>
             </ThemeProvider>
           </FormControl>
