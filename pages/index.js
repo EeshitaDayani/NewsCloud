aac91808bfd14a7787d3ee82bdcdbc8aceeff4ad
Eeshita Dayani <eeshitadayani@gmail.com>
2023-12-01 01:15:00 -0500

    Created new Navbar component to modularize code in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 import ThreeScene from '../src/components/ThreeScene';
-import InputField from '../src/components/InputField';
-import DateRangeSelector from '../src/components/DateRangeSelector';
+import TopNavbar from '../src/components/TopNavbar';
 import InstructionsPopup from '../src/components/InstructionsPopup';
 import ColorScale from '../src/components/ColorScale';
 
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import CssBaseline from '@mui/material/CssBaseline';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(true); // Initial font size
   const [searchQuery, setSearchQuery] = useState('');
   const [date, setDate] = useState('lastWeek');
-
-  const handleEnter = (inputValue) => {
-    setSearchQuery(inputValue);
-  };
-
-  const handleSelect = (inputValue) => {
-    setDate(inputValue);
-  };
-
-  const router = useRouter();
-  const handleRefresh = () => {
-    router.reload();
-  };
-
-  const [isMobile, setIsMobile] = useState(true); // Initial font size
 
   useEffect(() => {
     const calculateScreenSize = () => {
@@ -61,40 +41,7 @@ export default function Home() {
         <ThreeScene searchQuery={searchQuery} date={date} isMobile={isMobile} />
       </div>
       <InstructionsPopup/>
-      <div className={styles.contentContainer}>
-        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' }, '*': { userSelect: 'none' } }} />
-        <CssBaseline />
-
-        <Toolbar className={styles.toolbar}>
-          <div>
-            <div className={styles.title} onClick={handleRefresh}>
-              <Typography variant='h3' noWrap fontFamily={'Inter'}>
-                <span className={styles.titlePartOne}>
-                  NEWS
-                </span>
-                <span className={styles.titlePartTwo}>
-                  CLOUD
-                </span>
-              </Typography>
-            </div>
-            <div className={styles.subtitle} onClick={handleRefresh}>
-              <Typography variant='h6' noWrap fontFamily={'Inter'}>
-                <span className={styles.titlePartTwo}>
-                YOUR DAILY NEWS SNAPSHOT
-                </span>
-              </Typography>
-            </div>
-          </div>
-          <div className={styles.inputContainer}>
-            <div className={styles.inputField}>
-              <InputField onEnter={handleEnter} />
-            </div>
-            <div className={styles.dateSelector}>
-              <DateRangeSelector onSelect={handleSelect} />
-            </div>
-          </div>
-        </Toolbar>
-      </div>
+      <TopNavbar setSearchQuery={setSearchQuery} setDate={setDate} />
       {/* <ColorScale /> TODO: Uncomment once hover text is implemented */}
       <GitHubIcon className={styles.github} onClick={() => window.open('https://github.com/EeshitaDayani/NewsCloud_v2', '_blank')} />
     </div>
---
+++ src/components/TopNavbar.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import InputField from '@/src/components/InputField';
+import DateRangeSelector from '@/src/components/DateRangeSelector';
+import InstructionsPopup from '@/src/components/InstructionsPopup';
+
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import CssBaseline from '@mui/material/CssBaseline';
+import styles from '@/styles/TopNavbar.module.css';
+
+export default function TopNavbar({ setSearchQuery, setDate }) {
+    const handleEnter = (inputValue) => {
+        setSearchQuery(inputValue);
+      };
+    
+      const handleSelect = (inputValue) => {
+        setDate(inputValue);
+      };
+    
+      const router = useRouter();
+      const handleRefresh = () => {
+        router.reload();
+      };
+
+    return (
+      <div className={styles.contentContainer}>
+        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' }, '*': { userSelect: 'none' } }} />
+        <CssBaseline />
+
+        <Toolbar className={styles.toolbar}>
+          <div>
+            <div className={styles.title} onClick={handleRefresh}>
+              <Typography variant='h3' noWrap fontFamily={'Inter'}>
+                <span className={styles.titlePartOne}>
+                  NEWS
+                </span>
+                <span className={styles.titlePartTwo}>
+                  CLOUD
+                </span>
+              </Typography>
+            </div>
+            <div className={styles.subtitle} onClick={handleRefresh}>
+              <Typography variant='h6' noWrap fontFamily={'Inter'}>
+                <span className={styles.titlePartTwo}>
+                YOUR DAILY NEWS SNAPSHOT
+                </span>
+              </Typography>
+            </div>
+          </div>
+          <div className={styles.inputContainer}>
+            <div className={styles.inputField}>
+              <InputField onEnter={handleEnter} />
+            </div>
+            <div className={styles.dateSelector}>
+              <DateRangeSelector onSelect={handleSelect} />
+            </div>
+          </div>
+        </Toolbar>
+      </div>
+    )
+}
